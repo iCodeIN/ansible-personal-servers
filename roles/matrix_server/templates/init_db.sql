01a33301875471107ba11741011a39c3e493216b
Anastassios Martakos <anastassios.martakos@outlook.com>
2021-02-19 21:30:12 +0100

    Add matrix telegram bridge

--- roles/matrix_server/templates/init_db.sql
+++ roles/matrix_server/templates/init_db.sql
@@ -1,6 +1,9 @@
-CREATE DATABASE {{ matrix_server_db_user }}
+CREATE DATABASE {{ matrix_server_db_name }}
   ENCODING 'UTF8'
   LC_COLLATE='C'
   LC_CTYPE='C'
   template=template0
+  OWNER {{ matrix_server_db_user }};
+
+CREATE DATABASE {{ matrix_server_db_telegram_name }}
   OWNER {{ matrix_server_db_user }};
