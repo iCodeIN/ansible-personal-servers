fcdebecb055db3c194be1c69343f2afc21175340
Anastassios Martakos <anastassios.martakos@outlook.com>
2021-08-11 22:51:33 +0200

    Add role to sync onedrive

---
+++ roles/onedrive/templates/sync_onedrive.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+docker container inspect {{ onedrive_container_name }} > /dev/null 2> /dev/null || \
+  docker run -d --rm \
+    --name {{ onedrive_container_name }} \
+    -v {{ onedrive_installation_dir }}/rclone:/config/rclone \
+    -v {{ onedrive_data_dir }}:/data \
+    rclone/rclone copy -P \
+      --checkers=28 \
+      --transfers=28 \
+      onedrive:/ /data
